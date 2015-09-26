427569057b0b1afd46bd6fae2ee6f6a83333ea46
Jason Wu <jasonwupilly1997@gmail.com>
2015-09-26 04:36:54 -0400

    view library view

--- flashlearn/flashcards/urls.py
+++ flashlearn/flashcards/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^document/(?P<document_id>[0-9]+)/$', views.view_document, name='view_document'),
     url(r'^document/(?P<document_id>[0-9]+)/edit/$', views.edit_document, name='edit_document'),
     url(r'^scan/upload/$', views.upload_scan, name="upload_scan"),
+    url(r'^library/$', views.view_library, name="view_library"),
 ]
