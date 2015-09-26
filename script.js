3de9f808f2928c31b23afe6cc76630760b6a1560
CaylaVinzons <cayla.vinzons@gmail.com>
2015-09-26 13:01:41 -0400

    home page makes some progress

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
    $( "#text:hidden:first" ).fadeIn( 3000 );
    $( "#slide1 img:hidden:first").fadeIn(6000);
    $("#slide1 img:hidden:first").slideLeft();
+   ${}
 });
