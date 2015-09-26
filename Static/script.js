310da73dcec3aa0e0444bc9ba8fe9a4127c86582
Marisa Hoenig <marisa.hoenig@gmail.com>
2015-09-26 19:33:18 -0400

    final homepage animation

--- Static/script.js
+++ Static/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
   $("#circle1").hide()
-  $("#circle1").fadeIn(2000, 'swing');
-  $("#arrow").hide()
-  $("#arrow").fadeIn(4000, 'swing');
-  $("#circle2").hide()
-  $("#circle2").fadeIn(5000, 'swing');
-
-
+  $("#circle1").fadeIn(1000, 'swing');
+  second();
+  third();
 });
+
+function second() {
+  $("#arrow").css({opacity: 0});
+  $("#arrow").delay(1000);
+  $("#arrow").fadeTo(1000, 1, 'swing');
+}
+
+function third() {
+  $("#circle2").css({opacity: 0});
+  $("#circle2").delay(2000);
+  $("#circle2").fadeTo(1000, 1, 'swing');
+}
