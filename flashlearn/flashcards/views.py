02b62e00b29c6636e752e0dddbc56c46414d6b70
Jason Wu <jasonwupilly1997@gmail.com>
2015-09-26 00:03:57 -0400

    add more stuff

--- flashlearn/flashcards/views.py
+++ flashlearn/flashcards/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
+
+from .models import *
 
 from django.http import HttpResponse
 
@@ -6,10 +9,18 @@ def index(request):
     return HttpResponse("You are at the flashcards page.")
 
 def view_card(request, card_id):
-    return HttpResponse("You are viewing card %s." % card_id)
+    try:
+        card = Card.objects.get(pk=card_id)
+    except Card.DoesNotExist:
+        raise Http404("Card doesn't exist")
+    return render(request, "flashcards/card.html", {'card': card})
 
 def view_document(request, document_id):
-    return HttpResponse("You are viewing document %s." % document_id)
+    try:
+        document = Document.objects.get(pk=document_id)
+    except Document.DoesNotExist:
+        raise Http404("Document doesn't exist")
+    return render(request, "flashcards/document.html", {'document': document})
 
 def edit_card(request, card_id):
     return HttpResponse("You are editing card %s." % card_id)
