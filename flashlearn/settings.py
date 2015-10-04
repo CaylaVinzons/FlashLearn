f962bc74f8829b4a7f04f15f5d8a9b22c68fa473
Jason Wu <jasonwupilly1997@gmail.com>
2015-10-04 03:03:59 -0400

    change static path

--- flashlearn/settings.py
+++ flashlearn/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT='static/'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'flashlearn/static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
